66ca592f433d12c734fdff0bfc1f36d5d536bb46
liminghao <liminghao@xiaomi.com>
2016-08-24 16:11:12 +0800

    Android.mk: include boot/Android.mk.
    N/A
    
    Signed-off-by: liminghao <liminghao@xiaomi.com>

--- Android.mk
+++ Android.mk
@@ -20,8 +20,13 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := fstab.bcm
+LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := $(LOCAL_MODULE)
 LOCAL_MODULE_CLASS := ETC
 LOCAL_MODULE_PATH := $(TARGET_ROOT_OUT)
 
 include $(BUILD_PREBUILT)
+
+########################################
+
+include $(LOCAL_PATH)/boot/Android.mk
