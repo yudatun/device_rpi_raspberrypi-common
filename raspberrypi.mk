709aa064b9a749a5ea865c80093e11d89f91e90b
liminghao <liminghao@xiaomi.com>
2016-10-13 09:33:20 +0800

    raspberrypi.mk: add the neccessary common products.
    N/A
    
    Signed-off-by: liminghao <liminghao@xiaomi.com>

---
+++ raspberrypi.mk
@@ -0,0 +1,10 @@
+PRODUCT_PACKAGES += \
+    cmdline.txt \
+    config.txt \
+    fstab.bcm \
+    mkdosfs
+
+PRODUCT_PACKAGES += debuggerd
+PRODUCT_PACKAGES += dump_syms
+
+PRODUCT_PACKAGES += gpio-rpi
