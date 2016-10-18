361caf5b2cf75f4ac174e4d998120e92e38c205b
liminghao <liminghao@xiaomi.com>
2016-10-18 17:01:33 +0800

    add shill_setup_wifi into PRODUCT_PACKAGES.
    N/A
    
    Signed-off-by: liminghao <liminghao@xiaomi.com>

--- raspberrypi.mk
+++ raspberrypi.mk
@@ -4,7 +4,9 @@ PRODUCT_PACKAGES += \
     fstab.bcm \
     mkdosfs
 
-PRODUCT_PACKAGES += debuggerd
-PRODUCT_PACKAGES += dump_syms
+PRODUCT_PACKAGES += \
+    debuggerd \
+    dump_syms \
+    shill_setup_wifi
 
 PRODUCT_PACKAGES += gpio-rpi
