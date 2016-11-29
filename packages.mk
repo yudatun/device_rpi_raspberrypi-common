8256fb8edd5820d36530d4563c120693a90a5074
liminghao <liminghao@xiaomi.com>
2016-11-29 17:32:34 +0800

    raspberrypi-common: add all of PRODUCT_PACKAGES into packages.mk
    N/A
    
    Signed-off-by: liminghao <liminghao@xiaomi.com>

---
+++ packages.mk
@@ -0,0 +1,11 @@
+PRODUCT_PACKAGES += \
+    debuggerd \
+    dump_syms \
+    cmdline.txt \
+    config.txt \
+    fstab.bcm \
+    getcap \
+    gpiorpi \
+    mkdosfs \
+    setcap \
+    shill_setup_wifi \
--- raspberrypi.mk
+++ raspberrypi.mk
@@ -1,11 +1,2 @@
-PRODUCT_PACKAGES += \
-    debuggerd \
-    dump_syms \
-    cmdline.txt \
-    config.txt \
-    fstab.bcm \
-    getcap \
-    gpiorpi \
-    mkdosfs \
-    setcap \
-    shill_setup_wifi \
+########################################
+include device/rpi/raspberrypi-common/packages.mk
